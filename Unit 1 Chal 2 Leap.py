28bdb30ada86ffdb27aca107736602024b501f5b
mahahariram <145341806+mahahariram@users.noreply.github.com>
2023-10-03 14:36:54 +0530

    Add files via upload

---
+++ Unit 1 Chal 2 Leap.py
@@ -0,0 +1,15 @@
+class Player:
+  def play(self):
+    print("The player is playing cricket.")
+class Batsman (Player):
+  def play(self):
+    print("The batsman is batting.")
+class Bowler (Player):
+  print("The bowler is bowling.")
+  
+if __name__ == "___main___":
+  batsman = Batsman()
+  bowler Bowler()
+  batsman.play()
+  def play(self):
+    bowler.play()
